f139f49a16033ac004ab46cd141f6cf9446bc2d2
gerardfp <gerardfp@gmail.com>
2023-01-25 23:32:18 +0100

    .

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -13,46 +13,45 @@ import java.util.List;
 import java.util.Random;
 
 class Cuadrito {
-	float x, y, vx = 0, vy = 0;
-	TextureRegion textureRegion;
-	static Texture atlas = new Texture("atlas.png");
-	static Random random = new Random();
+    float x, y, vx, vy;
+    TextureRegion textureRegion;
+    static Texture atlas = new Texture("atlas.png");
+    static Random random = new Random();
 
-	public Cuadrito(int x, int y, int regionX, int regionY) {
-		this.x = x;
-		this.y = y;
-		this.textureRegion = new TextureRegion(atlas, regionX,regionY,64,64);
-	}
+    public Cuadrito(int x, int y, int regionX, int regionY) {
+        this.x = x;
+        this.y = y;
+        this.textureRegion = new TextureRegion(atlas, regionX, regionY, 64, 64);
+    }
 
-	void move(){
-		x = MathUtils.clamp(x+vx, 0, Gdx.graphics.getWidth()-64);
-		vx = MathUtils.clamp(vx+random.nextFloat()-0.5f, -2, 2);
-
-		y = MathUtils.clamp(y+vy, 0, Gdx.graphics.getHeight()-64);
-		vy = MathUtils.clamp(vy+random.nextFloat()-0.5f, -2, 2);
-	}
+    void move() {
+        x = MathUtils.clamp(x + (vx = MathUtils.clamp(vx + random.nextFloat() - 0.5f, -2, 2)), 0, Gdx.graphics.getWidth() - 64);
+        y = MathUtils.clamp(y + (vy = MathUtils.clamp(vy + random.nextFloat() - 0.5f, -2, 2)), 0, Gdx.graphics.getHeight() - 64);
+    }
 }
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	List<Cuadrito> cuadritos = new ArrayList<>();
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		cuadritos = new ArrayList<>();
-		cuadritos.add(new Cuadrito(0,0, 0, 0));
-		cuadritos.add(new Cuadrito(100,0, 0,64));
-		cuadritos.add(new Cuadrito(0,100,  0,128));
-		cuadritos.add(new Cuadrito(100,100,  0,192));
-	}
+    SpriteBatch batch;
+    List<Cuadrito> cuadritos;
 
-	@Override
-	public void render () {
-		cuadritos.forEach(Cuadrito::move);
-		ScreenUtils.clear(1, 1, 1, 1);
-		batch.begin();
-		cuadritos.forEach(cuadrito -> batch.draw(cuadrito.textureRegion, cuadrito.x, cuadrito.y));
-		batch.end();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+
+        cuadritos = new ArrayList<>();
+        cuadritos.add(new Cuadrito(0, 0, 0, 0));
+        cuadritos.add(new Cuadrito(100, 0, 0, 64));
+        cuadritos.add(new Cuadrito(0, 100, 0, 128));
+        cuadritos.add(new Cuadrito(100, 100, 0, 192));
+    }
+
+    @Override
+    public void render() {
+        cuadritos.forEach(Cuadrito::move);
+
+        ScreenUtils.clear(1, 1, 1, 1);
+        batch.begin();
+        cuadritos.forEach(cuadrito -> batch.draw(cuadrito.textureRegion, cuadrito.x, cuadrito.y));
+        batch.end();
+    }
 }
